1d379433c055236d1b55a395b80e9445af40f528
Ferris Thomas <fsthomas@gmail.com>
2016-11-22 14:58:07 -0500

    Adds add to cart options. Adds clear all option. Adds cookie-ing. Adds history. Fixes issue with sale prices, filtering on category 74. Adds switch in inventory retrieval process whereby if you’ve got something previously cookied, you will be redirected to the site with those items already selected and move you to the stack page from select.

--- core/store.js
+++ core/store.js
@@ -15,11 +15,17 @@ import {
     UPDATE_INVENTORY,
     REMOVE_ITEM,
     GET_INVENTORY_RESPONSE, GET_INVENTORY_ERROR, GET_INVENTORY,
-    GET_PRICE, GET_PRICE_RESPONSE
+    GET_PRICE, GET_PRICE_RESPONSE,
+    ADD_TO_CART, ADD_TO_CART_RESPONSE, ADD_TO_CART_ERROR,
+    CLEAR_ALL_ITEMS
 } from '../core/action-types';
-import { INVENTORY_IMAGES, SALE_PERCENTAGE } from '../core/constants';
+import {INVENTORY_IMAGES, SALE_PERCENTAGE} from '../core/constants';
 import _ from '../node_modules/lodash';
 import request from 'superagent';
+import Cookies from '../node_modules/js-cookie/src/js.cookie.js';
+import history from './history';
+
+const COOKIE_NAME = 'com.shellybrown';
 
 // Centralized application state
 // For more information visit http://redux.js.org/
@@ -32,7 +38,80 @@ const initialState = {
 
 const store = createStore((state = initialState, action) => {
     // TODO: Add action handlers (aka "reduces")
+
+    // console.log('action.type: ' + action.type);
+
     switch (action.type) {
+
+        case CLEAR_ALL_ITEMS:
+
+            Cookies.set(COOKIE_NAME, {
+                    selectedProducts: []
+                },
+                {
+                    expires: 7
+                }
+            );
+
+            return {
+                ...state, inventory: state.inventory.map((item)=> {
+                    return {...item, selected: false}
+                }), stack: [], enoughSelected: false
+            };
+
+        case ADD_TO_CART:
+
+            var URLs = {
+                    "bubbleUp": "https://www.shellybrownstore.com/addbysku/index/index?skus=", // URL Provided by BubbleUp (triggers 301 redirect)
+                    "redirect": "https://store.shellybrown.com/addbysku/index/index?skus=", // URL Targeted in redirect
+                    "internalAPI": "https://www.shellybrownstore.com/api.php?action=addToCart&skus="
+                }, url = URLs.redirect,
+                productSKUs = state.stack.map((item)=> {
+                    return item.sku;
+                });
+
+            url += productSKUs.join(',');
+
+            request
+                .get(url)
+                .withCredentials()
+                .end((err, res) => {
+
+                    if (err) {
+                        /*
+                         in case there is any error, dispatch an action containing the error
+                         */
+                        console.warn('ADD_TO_CART Error:');
+                        console.log(err);
+                        store.dispatch({type: ADD_TO_CART_ERROR, err});
+                    } else {
+
+                        var response = JSON.parse(res.text);
+                        window.location = response.url_cart;
+
+                        /*store.dispatch({
+                         type: ADD_TO_CART_RESPONSE,
+                         data: JSON.parse(res.text)
+                         });
+                         */
+
+                    }
+                });
+
+            return {...state, processingStoreRequest: true};
+
+        /*
+         case ADD_TO_CART_RESPONSE:
+
+         console.log('ADD_TO_CART_RESPONSE', action.data.url_cart);
+
+         return {
+         ...state,
+         urlCart: action.data.url_cart,
+         urlCheckout: action.data.url_checkout
+         };
+         */
+
         case GET_PRICE:
 
             state.stack.map((item)=> {
@@ -40,36 +119,36 @@ const store = createStore((state = initialState, action) => {
                 if (item.price == 0) {
 
                     // Is the item currently on sale?
-                    var onSale = _.find(item.category_ids, item=>{
-                        return item==="74";
+                    var onSale = _.find(item.category_ids, item=> {
+                        return item === "74";
                     });
 
                     request
-                            .get('http://shellybrown.com/api.php?action=msrp&productID=' + item.product_id)
-                            .end((err, res) => {
+                        .get('http://shellybrown.com/api.php?action=msrp&productID=' + item.product_id)
+                        .end((err, res) => {
 
-                                if (err) {
-                                    /*
-                                     in case there is any error, dispatch an action containing the error
-                                     */
-                                    console.warn('Price Retrieval Error:');
-                                    console.log(err);
+                            if (err) {
+                                /*
+                                 in case there is any error, dispatch an action containing the error
+                                 */
+                                console.warn('Price Retrieval Error:');
+                                console.log(err);
 
-                                    store.dispatch({type: GET_INVENTORY_ERROR, err});
+                                store.dispatch({type: GET_INVENTORY_ERROR, err});
 
-                                } else {
+                            } else {
 
-                                    const data = JSON.parse(res.text);
+                                const data = JSON.parse(res.text);
 
-                                    /*
-                                     Once data is received, dispatch an action telling the application 
-                                     that data was received successfully, along with the parsed data 
-                                     */
+                                /*
+                                 Once data is received, dispatch an action telling the application 
+                                 that data was received successfully, along with the parsed data 
+                                 */
 
-                                    store.dispatch({type: GET_PRICE_RESPONSE, data});
-                                }
+                                store.dispatch({type: GET_PRICE_RESPONSE, data});
+                            }
 
-                            });
+                        });
 
                 }
             });
@@ -84,7 +163,7 @@ const store = createStore((state = initialState, action) => {
                 newInventory,
                 newStack;
 
-            if(data.on_sale == 1){
+            if (data.on_sale == 1) {
                 updatePrice -= (updatePrice * (SALE_PERCENTAGE / 100));
             }
 
@@ -104,7 +183,7 @@ const store = createStore((state = initialState, action) => {
 
             return {...state, inventory: newInventory, stack: newStack};
 
-          case GET_INVENTORY:
+        case GET_INVENTORY:
 
             request
                 .get('http://shellybrown.com/api.php?action=inventory')
@@ -141,33 +220,62 @@ const store = createStore((state = initialState, action) => {
                 itemFound = false,
                 i;
 
-            // Cycle over all the products we have images for looking for matches:
+            // Grab any items that were previously checked.
+            var cookie = Cookies.get(COOKIE_NAME) ? JSON.parse(Cookies.get(COOKIE_NAME)) : {},
+                savedStack = cookie.selectedProducts || [];
 
-            existingInventory.map(item=>{
+            // Cycle over all the products we have images for looking for matches:
+            existingInventory.map(item=> {
 
                 itemFound = false;
 
-                for(i = 0; i < INVENTORY_IMAGES.length; i++){
+                for (i = 0; i < INVENTORY_IMAGES.length; i++) {
 
-                    if(INVENTORY_IMAGES[i].sku == item.sku){
+                    if (INVENTORY_IMAGES[i].sku == item.sku) {
                         itemFound = true;
                         break;
                     }
                 }
 
-                if(itemFound){
-                    newInventory.push(Object.assign({}, item, INVENTORY_IMAGES[i]));
-                }else{
+                if (itemFound) {
+
+                    var itemDataWithImage = INVENTORY_IMAGES[i];
+                    itemDataWithImage.selected = _.find(savedStack, (stackItem)=> {
+                        return item.product_id === stackItem.product_id;
+                    });
+
+                    newInventory.push(Object.assign({}, item, itemDataWithImage));
+
+                } else {
                     inventoryMissingImages.push(Object.assign({}, item, INVENTORY_IMAGES[i]));
                 }
 
             });
 
-            _.each(inventoryMissingImages, (item)=>{
+            _.each(inventoryMissingImages, (item)=> {
                 console.warn('Image missing for item #' + item.product_id, item.sku, item.name);
             });
 
-            return {...state, inventory: newInventory, stack: []};
+            state = {...state,
+                inventory: newInventory,
+                stack: savedStack,
+                enoughSelected: savedStack.length >= 2,
+                processingStoreRequest: false,
+                urlCart: ''
+            };
+
+            if(savedStack.length > 0){
+                // Coming from a previous saved state
+                // or social share; go to the arrange page
+
+                history.push({pathname: "/arrange"});
+
+                return state;
+
+
+            }else{
+                return state;
+            }
 
         case SET_INVENTORY:
 
@@ -206,9 +314,26 @@ const store = createStore((state = initialState, action) => {
 
             newState.enoughSelected = newState.stack.length >= 2;
 
+            newState.processingStoreRequest = false;
+
+            // Clear the reference to the cart if it previously existed.
+            // This should re-set the Add to Cart Button.
+            newState.urlCart = '';
+
+            Cookies.set(COOKIE_NAME, {
+                    selectedProducts: newState.stack.map(item=> {
+                        return item
+                    })
+                },
+                {
+                    expires: 7
+                }
+            );
+
             return newState;
 
-        case UPDATE_INVENTORY:
+        case
+        UPDATE_INVENTORY:
 
             var newStack = action.items,
                 newInventory = state.inventory.map(item=> {
@@ -226,7 +351,8 @@ const store = createStore((state = initialState, action) => {
                 stack: newStack
             };
 
-        case REMOVE_ITEM:
+        case
+        REMOVE_ITEM:
 
             var newStack = [],
                 stackOrderOffset = -1,
