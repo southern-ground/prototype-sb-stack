ce014b308cd8acc4907db3c1ace38622b1ef1678
Ferris Thomas <fsthomas@gmail.com>
2016-11-29 09:09:33 -0500

    Adds constants migrated from store.

--- core/constants.js
+++ core/constants.js
@@ -2,3 +2,6 @@
  * Created by fst on 11/14/16.
  */
 export const SALE_PERCENTAGE = 20;
+export const COOKIE_NAME = 'com.shellybrown';
+export const API_URL = 'http://shellybrown.com/api.php';
+export const IMAGE_DATA_URL = "/data/stackItems.json";
