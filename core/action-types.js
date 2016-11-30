a3da69547e5ac4e39090f0d70b3063f50c67acdb
Ferris Thomas <fsthomas@gmail.com>
2016-11-30 17:12:18 -0500

    Adds the stack order const and removes remove.

--- core/action-types.js
+++ core/action-types.js
@@ -2,6 +2,7 @@
  * Created by fst on 8/16/16.
  */
 export const SET_INVENTORY = "action-set-inventory";
+export const SET_STACK_ORDER = "action-set-stack-order";
 export const GET_INVENTORY = "action-get-inventory";
 export const GET_INVENTORY_ERROR = "action-get-inventory-error";
 export const GET_INVENTORY_RESPONSE = "action-get-inventory-response";
@@ -11,7 +12,6 @@ export const GET_PRICE = "action-get-price";
 export const GET_PRICE_RESPONSE = "action-get-price-response";
 export const UPDATE_INVENTORY = "action-update-inventory";
 export const TOGGLE_ITEM = "action-toggle-selected";
-export const REMOVE_ITEM = "action-remove-item";
 export const ADD_TO_CART = "action-add-to-cart";
 export const ADD_TO_CART_ERROR = "action-add-to-cart-error";
 export const CLEAR_ALL_ITEMS = "action-clear-all-items";
