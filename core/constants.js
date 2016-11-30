414f1aa772b56dea7e3aa7ed8a604631465be421
Ferris Thomas <fsthomas@gmail.com>
2016-11-30 17:12:42 -0500

    Adds constant for min number of selected items.

--- core/constants.js
+++ core/constants.js
@@ -1,7 +1,8 @@
 /**
  * Created by fst on 11/14/16.
  */
-export const SALE_PERCENTAGE = 20;
+export const SALE_PERCENTAGE = 20; // % Discount on sale items. Thanks, Bubble Up.
+export const NUM_SELECTED_REQUIRED = 2; // Required number of products selected to trigger arrange screen.
 export const COOKIE_NAME = 'com.shellybrown';
 export const BUBBLE_UP_API_URL = 'https://store.shellybrown.com/addbysku/index/index?skus=';
 export const API_URL = 'http://shellybrown.com/api.php';
